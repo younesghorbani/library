4c6a1deffa9aed346656227085f93baffda57bac
Younes Ghorbani <122295811+younesghorbani@users.noreply.github.com>
2023-06-09 11:48:08 +0330

    Add adding new book functionality

--- script.js
+++ script.js
@@ -8,10 +8,31 @@ function closeModal() {
     overlay.classList.add('hidden');
 }
 
+function Book(title, authors, pages, read) {
+    this.title = title;
+    this.authors = authors;
+    this.pages = pages;
+    this.read = read;
+}
+
+let library = [];
+function addBook(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('#book-title').value;
+    const authors = document.querySelector('#book-authors').value;
+    const pages = document.querySelector('#book-pages').value;
+    const read = Boolean(document.querySelector('#book-read').checked);
+    
+    const book = new Book(title, authors, pages, read);
+    library.push(book);
+}
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const newBtn = document.querySelector('.new');
 const closeBtn = document.querySelector('.close');
+const addBtn = document.querySelector('.add');
 
 newBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
@@ -19,3 +40,4 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', event => {
     if (event.key === 'Escape') closeModal();
 });
+addBtn.addEventListener('click', addBook);
